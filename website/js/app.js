46fc5d564bf9b9148bd213326a421d66aa5f2283
mMoukhtar <mohamed.moukhtar@outlook.com>
2020-12-20 18:47:06 +0200

    Fet: Implement Events and Main Functions
    Add event listeners and even handlers
    Add getCurrentWeather using ASYNC GET request from openWeatherMap API
    Add addWeatherEntry using ASYNC POST request to post new entries to server.js endPoint 'new'

--- website/js/app.js
+++ website/js/app.js
@@ -1,7 +1,42 @@
-const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+// Local Variables
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
 const apiKey = '96d15f9c58b0922548c54c6a9bbbe196';
 
+// Event Listeners
+document.getElementById('generate').addEventListener('click', generateBtnOnClick)
 
+
+// Events Handlers
+function generateBtnOnClick(event) {
+    const zipCode = document.getElementById('zip').value;
+    const countryCode = document.getElementById('countries').value;
+    const feeling = document.getElementById('feeling').value;
+    getCurrentWeather(zipCode, countryCode, feeling);
+}
+//49546
+// Main Functions
+
+// Add new Weather Entry
+
+function addWeatherEntry(weatherNewEntry) {
+    postData('/new', weatherNewEntry).then(data => {
+        console.log(`new entry posted: 
+        ${JSON.stringify(data)}`);
+        //getData('/all').then(data => console.log(`all data = ${JSON.stringify(data)}`));
+    });
+}
+
+//Open Weather Map API
+
+function getCurrentWeather(zipCode, countryCode, feeling) {
+    getData(`${baseURL}?zip=${zipCode},${countryCode}&appid=${apiKey}`).then(data => {
+        const newEntry = { temperature: data.main.temp, date: Date(), feeling: feeling };
+        addWeatherEntry(newEntry);
+    });
+}
+
+
+// ASYNC Functions
 const postData = async (url = '', data = {}) => {
     const response = await fetch(url, {
         method: 'POST',
@@ -29,9 +64,9 @@ const getData = async (url = '') => {
     }
 }
 
-postData('/new', { temperature: 21, date: Date(), response: 'some response' }).then(data => {
-    console.log(data);
-    getData('/all').then(data => console.log(`all data = ${JSON.stringify(data)}`));
-});
+// postData('/new', { temperature: 21, date: Date(), response: 'some response' }).then(data => {
+//     console.log(data);
+//     getData('/all').then(data => console.log(`all data = ${JSON.stringify(data)}`));
+// });
 
 
