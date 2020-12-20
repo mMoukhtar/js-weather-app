1a4c3e995c6dec96f1835284f25b492f3f5a2f77
mMoukhtar <mohamed.moukhtar@outlook.com>
2020-12-20 14:05:22 +0200

    Fet: Add Async Functionality
    Add ASYNC getData and setData methods to App.Js
    getData is an async Fetch method takes url of an endPoint
    postData is an async Fetch method Post data to an endPoint

---
+++ website/js/app.js
@@ -0,0 +1,31 @@
+const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '96d15f9c58b0922548c54c6a9bbbe196';
+
+
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+const getData = async (url = '') => {
+    const response = await fetch(url);
+    try {
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
